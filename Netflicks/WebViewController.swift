134e45f1ba1e47db6855a2cd67e3cc4e19e3b8cb
MattxDamon <mbmattbarth@gmail.com>
2015-07-30 11:26:08 -0400

    cleaned up youtube viewer and detail auto layout

--- Netflicks/WebViewController.swift
+++ Netflicks/WebViewController.swift
@@ -14,53 +14,27 @@ class WebViewController: UIViewController, YouTubePlayerDelegate {
     @IBOutlet weak var playerView: YouTubePlayerView!
     @IBOutlet weak var playButton: UIButton!
     
-    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
-    
     var url: NSURL!
     
     //so it is able to play as soon as video loads
     func playerReady(videoPlayer: YouTubePlayerView) {
+//        self.activityIndicator.stopAnimating()
         playerView.play()
         playButton.setTitle("Pause", forState: .Normal)
-        
-        //Ending:
-        self.activityIndicator.stopAnimating()
-//        UIApplication.sharedApplication().endIgnoringInteractionEvents()
     }
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        activityIndicator = UIActivityIndicatorView(frame: CGRectMake(0,0,100,100))
-        activityIndicator.center = self.view.center
-        activityIndicator.hidesWhenStopped = true
-        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.White
-        view.addSubview(activityIndicator)
-        
-        //show video is loading
-        activityIndicator.startAnimating()
-//        UIApplication.sharedApplication().beginIgnoringInteractionEvents()
-        
         //setting up the video
         playerView.playerVars = ["playsinline": "1"]
         playerView.loadVideoURL(url!)
     }
     
-
-    @IBAction func playButtonPressed(sender: AnyObject) {
-        if playerView.ready {
-            if playerView.playerState != YouTubePlayerState.Playing {
-                playerView.play()
-                playButton.setTitle("Pause", forState: .Normal)
-            } else {
-                playerView.pause()
-                playButton.setTitle("Play", forState: .Normal)
-            }
-        }
-    }
     
     //noted by the author that these methods should be optional in the delegate
     func playerStateChanged(videoPlayer: YouTubePlayerView, playerState: YouTubePlayerState) {
+//        self.activityIndicator.stopAnimating()
     }
     func playerQualityChanged(videoPlayer: YouTubePlayerView, playbackQuality: YouTubePlaybackQuality) {
     }
